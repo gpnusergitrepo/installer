b1f7f582ff35e9766ea8fa5640abae522439fdba
gpnusergitrepo <154529841+gpnusergitrepo@users.noreply.github.com>
2023-12-21 19:29:08 +0500

    Create createdb.sql

---
+++ createdb.sql
@@ -0,0 +1,13 @@
+-- Database: ngcp
+
+-- DROP DATABASE IF EXISTS ngcp;
+
+CREATE DATABASE ngcp
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf-8'
+    LC_CTYPE = 'en_US.utf-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
